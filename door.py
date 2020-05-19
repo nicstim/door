4f72a954159b1ed74a413414ca8b40a31dc2b74d
Nicstim <56487341+nicstim@users.noreply.github.com>
2020-05-19 21:55:44 +0300

    Add files via upload

---
+++ door.py
@@ -0,0 +1,19 @@
+open_door = []
+close_door = []
+door = [False] * 100
+def walk(n): # Функция "прогулки"
+    for i in range(n,len(door),n+1): # Подходим к каждой N двери.
+        door[i] = not door[i]
+    return door
+
+for n in range(0,100,1):
+    door = walk(n)
+for i in range(0,len(door),1): # Проверям открыта дверь(True) или закрыта(False)
+    if door[i] == True:
+        close_door.append(i+1)
+    else:
+        open_door.append(i+1)
+print(f"{len(open_door)} Open door:")
+print(open_door)
+print(f"{len(close_door)} Close door:")
+print(close_door)
